c8c2eb7a7bb8c25a3489c32c69a83796d0a291e9
lensel14 <108508233+lensel14@users.noreply.github.com>
2022-11-23 19:07:41 +0530

    Add files via upload

---
+++ src/test/java/com/lenin/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenin.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
